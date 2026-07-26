2de16baabc29acec5d8f952c788b24cf6db19896
syauqihasibuan25-dot <syauqihasibuan25@gmail.com>
2026-07-26 13:26:49 +0800

    Update orbs.tsx

--- project-bolt-sb1-5irfvpkx/project/components/portfolio/orbs.tsx
+++ project-bolt-sb1-5irfvpkx/project/components/portfolio/orbs.tsx
@@ -1,3 +1,8 @@
 export function BackgroundOrbs() {
-  return null;
+  return (
+    <div className="absolute inset-0 overflow-hidden pointer-events-none">
+      <div className="absolute -top-40 -left-40 w-96 h-96 bg-purple-500/10 rounded-full blur-3xl" />
+      <div className="absolute top-1/2 -right-40 w-96 h-96 bg-blue-500/10 rounded-full blur-3xl" />
+    </div>
+  );
 }
