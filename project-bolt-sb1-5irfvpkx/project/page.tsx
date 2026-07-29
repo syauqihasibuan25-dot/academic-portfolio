0657b22f755c387a6456cea61d0cc5a2066e9a30
syauqihasibuan25-dot <syauqihasibuan25@gmail.com>
2026-07-30 00:12:08 +0800

    Update page.tsx

--- project-bolt-sb1-5irfvpkx/project/page.tsx
+++ project-bolt-sb1-5irfvpkx/project/page.tsx
@@ -1,7 +1,7 @@
 import { BackgroundOrbs } from '@/components/portfolio/orbs'
 import { Hero } from '@/components/portfolio/hero'
 import { Achievements } from '@/components/portfolio/achievements'
-import SustainableEnergySection from '@/components/portfolio/SustainableEnergySection';
+import { SustainableEnergySection } from '@/components/portfolio/SustainableEnergySection';
 
 export default function Page() {
   return (
